39cdf05787f32531cd5e74dc8908ef813b30e7ec
Xiao Ling <bardminz@gmail.com>
2014-11-10 15:33:17 -0800

    creates a separate test code directory and adds a few test cases

---
+++ test/HEXGraphTest.java
@@ -0,0 +1,130 @@
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author dgorrie
+ *         <p/>
+ *         Testing class for our HEXGraphImplementation
+ */
+public class HEXGraphTest {
+    private static HEXGraph<String> graph;
+    private static final String[] testNames = {"PERSON", "MOUNTAIN", "ANIMAL"};
+    private static final String[] exclusions = {"CAT, DOG", "ANIMAL, LAKE", "LAKE, MOUNTAIN"};
+    private static final String[] subsets = {"ARTIST, POLITICIAN", "", "PUPPY, PERSON, KITTEN, ARTIST, CAT, POLITICIAN, DOG"};
+    private static final String[] supersets = {"ANIMAL", "", ""};
+
+
+    @BeforeClass
+    public static void setup() throws IOException, IllegalStateException {
+        HEXGraphFactory factory = new HEXGraphFactory();
+        String filepath = "src/sample.hxg";
+        factory.buildHEXGraph(filepath);
+        graph = factory.getLiteralGraph(filepath);
+    }
+
+    @Test
+    public void testExclusions() {
+        for (int i = 0; i < testNames.length; i++) {
+            // System.out.println(graph.getExcluded(testNames[i]).toString());
+            assertEquals(graph.getExcluded(testNames[i]).toString(), "[" + exclusions[i] + "]");
+        }
+    }
+
+    @Test
+    public void testHierarchySubset() {
+        for (int i = 0; i < testNames.length; i++) {
+            // System.out.println(graph.getHierarchySubset(testNames[i]).toString());
+            assertEquals(graph.getHierarchySubset(testNames[i]).toString(), "[" + subsets[i] + "]");
+        }
+    }
+
+    @Test
+    public void testHierarchySuperset() {
+        for (int i = 0; i < testNames.length; i++) {
+            // System.out.println(graph.getHierarchySuperset(testNames[i]).toString());
+            assertEquals(graph.getHierarchySuperset(testNames[i]).toString(), "[" + supersets[i] + "]");
+        }
+    }
+
+    private HEXGraph<String> createSimpleGraph() {
+        HEXGraph<String> graph = new HEXGraph<String>();
+        graph.addNode("Person");
+        graph.addNode("Dog");
+        graph.addNode("Actor");
+        graph.addNode("Politician");
+        graph.addExclusion("Person", "Dog");
+        graph.addHierarchy("Person", "Actor");
+        graph.addHierarchy("Person", "Politician");
+        return graph;
+    }
+
+    @Test
+    public void testCreation() {
+        HEXGraph<String> graph = createSimpleGraph();
+        assertEquals(4, graph.getNodes().size());
+        assertEquals(HEXGraph.Relationship.EXCLUSION, graph.getRelationship("Dog", "Person"));
+        assertEquals(HEXGraph.Relationship.EXCLUSION, graph.getRelationship("Person", "Dog"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUPER, graph.getRelationship("Actor", "Person"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUPER, graph.getRelationship("Politician", "Person"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUB, graph.getRelationship("Person", "Actor"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUB, graph.getRelationship("Person", "Politician"));
+        assertEquals(HEXGraph.Relationship.OVERLAPPING, graph.getRelationship("Actor", "Politician"));
+        assertEquals(HEXGraph.Relationship.OVERLAPPING, graph.getRelationship("Politician", "Actor"));
+    }
+
+
+    @Test
+    public void testDensify() {
+        HEXGraph<String> graph = createSimpleGraph();
+        graph.densify();
+        assertEquals(HEXGraph.Relationship.EXCLUSION, graph.getRelationship("Dog", "Actor"));
+        assertEquals(HEXGraph.Relationship.EXCLUSION, graph.getRelationship("Dog", "Person"));
+        assertEquals(HEXGraph.Relationship.EXCLUSION, graph.getRelationship("Dog", "Politician"));
+        assertEquals(HEXGraph.Relationship.EXCLUSION, graph.getRelationship("Actor", "Dog"));
+        assertEquals(HEXGraph.Relationship.EXCLUSION, graph.getRelationship("Person", "Dog"));
+        assertEquals(HEXGraph.Relationship.EXCLUSION, graph.getRelationship("Politician", "Dog"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUPER, graph.getRelationship("Actor", "Person"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUPER, graph.getRelationship("Politician", "Person"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUB, graph.getRelationship("Person", "Actor"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUB, graph.getRelationship("Person", "Politician"));
+        assertEquals(HEXGraph.Relationship.OVERLAPPING, graph.getRelationship("Actor", "Politician"));
+        assertEquals(HEXGraph.Relationship.OVERLAPPING, graph.getRelationship("Politician", "Actor"));
+    }
+
+    @Test
+    public void testSparisfy() {
+        HEXGraph<String> graph = createSimpleGraph();
+        graph.sparsify();
+        // graph stays the same
+        assertEquals(HEXGraph.Relationship.EXCLUSION, graph.getRelationship("Dog", "Person"));
+        assertEquals(HEXGraph.Relationship.EXCLUSION, graph.getRelationship("Person", "Dog"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUPER, graph.getRelationship("Actor", "Person"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUPER, graph.getRelationship("Politician", "Person"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUB, graph.getRelationship("Person", "Actor"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUB, graph.getRelationship("Person", "Politician"));
+        assertEquals(HEXGraph.Relationship.OVERLAPPING, graph.getRelationship("Actor", "Politician"));
+        assertEquals(HEXGraph.Relationship.OVERLAPPING, graph.getRelationship("Politician", "Actor"));
+    }
+
+    @Test
+    public void testSparisfyAfterDensify() {
+        HEXGraph<String> graph = createSimpleGraph();
+        graph.densify();
+        graph.sparsify();
+        // graph stays the same
+        assertEquals(HEXGraph.Relationship.EXCLUSION, graph.getRelationship("Dog", "Person"));
+        assertEquals(HEXGraph.Relationship.EXCLUSION, graph.getRelationship("Person", "Dog"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUPER, graph.getRelationship("Actor", "Person"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUPER, graph.getRelationship("Politician", "Person"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUB, graph.getRelationship("Person", "Actor"));
+        assertEquals(HEXGraph.Relationship.HIERARCHY_SUB, graph.getRelationship("Person", "Politician"));
+        assertEquals(HEXGraph.Relationship.OVERLAPPING, graph.getRelationship("Actor", "Politician"));
+        assertEquals(HEXGraph.Relationship.OVERLAPPING, graph.getRelationship("Politician", "Actor"));
+    }
+
+}
